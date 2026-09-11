f7290d72f96276c1ee004484237bb6961d8d37b4
01609538815rocky-sudo <01609538815rocky@gmail.com>
2026-09-12 01:13:44 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '8830495719:AAEpKd2IaeKLelwFBte8aeKcQGo5_5J9jCg';
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'স্বাগতম! নিচের মিনি অ্যাপ বাটন থেকে আপনার কাজ শুরু করুন।', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text: '🚀 মিনি অ্যাপ ওপেন করুন',
+                        web_app: { url: 'https://bd-task-earning.vercel.app' }
+                    }
+                ]
+            ]
+        }
+    });
+});
+
+console.log('Bot is running successfully with new token...');
